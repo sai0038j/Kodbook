992a08a86ca66421fb7e0cbcf5baece87db7811b
sai0038j <160742371+sai0038j@users.noreply.github.com>
2024-11-04 16:51:08 +0530

    commit

--- src/main/java/com/kodbook/controllers/NavigationController.java
+++ src/main/java/com/kodbook/controllers/NavigationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NavigationController {
+	//push yfrrasdfghjkp[]poiuytre
 	@GetMapping("/")
 	public String index() {
 		return"index";
